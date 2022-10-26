f8510055f59eb79612c99c7cba4993eacd39e80a
Cristian González <cgonzalezhens@gmail.com>
2022-10-26 14:50:22 +0200

    meter filtros

---
+++ Gateway/src/main/java/com/gateway/Filtros.java
@@ -0,0 +1,50 @@
+package com.gateway;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import reactor.core.publisher.Mono;
+
+@Configuration
+public class Filtros {
+
+    //IMPORT LOG
+    private static Logger log = LoggerFactory.getLogger(Filtros.class);
+
+    //METER FILTROS
+    @Bean
+    @Order(-1)
+    public GlobalFilter a() {
+        return (exchange, chain) -> {
+            log.info("first pre filter");
+            return chain.filter(exchange).then(Mono.fromRunnable(() -> {
+                log.info("third post filter");
+            }));
+        };
+    }
+
+    @Bean
+    @Order(0)
+    public GlobalFilter b() {
+        return (exchange, chain) -> {
+            log.info("second pre filter");
+            return chain.filter(exchange).then(Mono.fromRunnable(() -> {
+                log.info("second post filter");
+            }));
+        };
+    }
+
+    @Bean
+    @Order(1)
+    public GlobalFilter C() {
+        return ((exchange, chain) -> {
+            log.info("third pre filter");
+            return chain.filter(exchange).then(Mono.fromRunnable(() -> {
+                log.info("first post filter");
+            }));
+        });
+    }
+}
